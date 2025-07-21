6d7d0f40712ea38f7ce975a7f2b55dbe01cac5e3
PhoopPeep <christina_c@cmu.ac.th>
2025-07-21 19:22:55 +0700

    edit unit test

--- backend/tests/global-testdown.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('../generated/prisma/client')
-
-module.exports = async function globalTeardown() {
-  console.log('Cleaning up test environment...')
-  const prisma = new PrismaClient()
-  await prisma.$disconnect()
-  console.log('Test cleanup completed')
-}
